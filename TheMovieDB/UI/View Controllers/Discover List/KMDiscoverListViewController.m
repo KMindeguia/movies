d1829b191fcfa84efb611f4c21d01cd4c0dbe309
khramtsou <vl.khramtsov@gmail.com>
2015-02-24 14:16:08 +0300

    added missing super calls

--- TheMovieDB/UI/View Controllers/Discover List/KMDiscoverListViewController.m
+++ TheMovieDB/UI/View Controllers/Discover List/KMDiscoverListViewController.m
@@ -40,6 +40,7 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
--- TheMovieDB/UI/View Controllers/Movies Grid View/KMSimilarMoviesViewController.m
+++ TheMovieDB/UI/View Controllers/Movies Grid View/KMSimilarMoviesViewController.m
@@ -44,6 +44,7 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:YES];
     [self setupCollectionViewLayout];
 }
